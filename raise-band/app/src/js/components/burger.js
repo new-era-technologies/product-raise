ecc03dc9c5e850171fc70c60fccb0a65171e4694
Vladymyr Radionov <radionovdon@gmail.com>
2021-03-25 13:16:40 +0200

    header + burger mob

--- raise-band/app/src/js/components/burger.js
+++ raise-band/app/src/js/components/burger.js
@@ -3,14 +3,17 @@
 //declare variables
 const burger = document.getElementById('burger'),
       burgerModal = document.getElementById('burger-modal'),
-      butCloseBurModal = document.getElementById('but-close-burger-modal');
+      butCloseBurModal = document.getElementById('but-close-burger-modal'),
+      burgerModalList = document.getElementById('burger-modal-list');
 
 //show burger nav menu
-burger.addEventListener('click', function() {
+burger.addEventListener('click', function () {
     burgerModal.style.width = '80%';
+    burgerModalList.style.display = 'flex';
 });
 
 //hide burger nav menu
-butCloseBurModal.addEventListener('click', function() {
+butCloseBurModal.addEventListener('click', function () {
     burgerModal.style.width = 0;
+    burgerModalList.style.display = 'none';
 });
