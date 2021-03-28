d3583d770562614504167ecba1c5ff19521ee45e
Vladymyr Radionov <radionovdon@gmail.com>
2021-03-28 10:15:44 +0300

    calendar2

--- raise-band/app/src/js/components/calculator.js
+++ raise-band/app/src/js/components/calculator.js
@@ -2,6 +2,7 @@
 
 //declare variables
 const dropBtn = document.querySelectorAll('.control__btn.dropbtn'),
+    dropCont = document.querySelectorAll('.dropdown-content'),
     menuItems = document.querySelectorAll('.control__list__item'),
     minPages = document.getElementById('min-pages'),
     maxPages = document.getElementById('max-pages'),
@@ -12,22 +13,23 @@ const dropBtn = document.querySelectorAll('.control__btn.dropbtn'),
 dropBtn.forEach(
     v => v.addEventListener('click', function (e) {
         this.closest('.dropdown').children[1].classList.toggle('show');
-        // e.target !== thisthis.closest('.dropdown').children[1] || e.target !== this ? this.closest('.dropdown').children[1].classList.remove('show') : null
     })
 )
 
 //hide menu by click out
-// window.addEventListener('click', function(e) {
-//     document.querySelectorAll('.show').forEach(
-//         v => e.target !== dropBtn ? v.classList.remove('show') : null
-//     );
-//     console.log(e.target);
-// });
+window.addEventListener('click', function (e) {
+    if (!e.target.classList.contains('dropbtn')) {
+        dropCont.forEach(
+            it => it.classList.remove('show')
+        )
+    }
+});
 
 //change value in dropdown button
 menuItems.forEach(
     it => it.addEventListener('click', function (e) {
         this.closest('.dropdown').children[0].innerText = this.innerText;
+        this.closest('.dropdown').children[1].classList.remove('show');
     })
 )
 
@@ -40,7 +42,7 @@ minPages.addEventListener('click', function () {
 
 //count pages by click +
 maxPages.addEventListener('click', function () {
-        pagesCount.innerHTML = Number(pagesCount.innerHTML) + 1 ;
+    pagesCount.innerHTML = Number(pagesCount.innerHTML) + 1;
 })
 
 
